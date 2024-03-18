9542806c59f502d3e9a6d1182aa777552bf2da44
jdhernandez <tecnologia@eduyocio.com>
2024-03-18 18:11:41 -0500

    cambio lenguaje y ajuste de validacion numero entero

--- src/esprimo/EsPrimo.java
+++ src/esprimo/EsPrimo.java
@@ -20,24 +20,26 @@ public class EsPrimo {
         String numberStr = JOptionPane.showInputDialog("ingrese numero entero positivo para validar si es primo");
         int number = Integer.parseInt(numberStr);
         if (number > 0) {
-            if (esPrimo(number)) {
-                System.out.println(number + " es primo.");
+            if (isPrime(number)) {
+                JOptionPane.showMessageDialog(null, number + " es primo.");
             } else {
-                System.out.println(number + " no es primo.");
+                JOptionPane.showMessageDialog(null, number + " no es primo.");
             }
+        } else {
+            JOptionPane.showMessageDialog(null, "el numero debe ser mayor a 0");
         }
     }
 
-    public static boolean esPrimo(int numero) {
+    public static boolean isPrime(int number) {
         // Los números menores o iguales a 1 no son primos
-        if (numero <= 1) {
+        if (number <= 1) {
             return false;
         }
 
         // Recorremos todos los números desde 2 hasta la mitad de 'numero'
-        for (int i = 2; i <= numero / 2; i++) {
+        for (int i = 2; i <= number / 2; i++) {
             // Si encontramos un divisor exacto, el número no es primo
-            if (numero % i == 0) {
+            if (number % i == 0) {
                 return false;
             }
         }
